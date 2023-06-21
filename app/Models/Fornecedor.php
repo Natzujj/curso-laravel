ba2ffb8cb283cf0255000bda56e5abd87a951563
Natzujj <gabrielnatsujake@gmail.com>
2023-06-21 19:04:54 -0300

    Ajustando nome da tabela no MODEL para o ORM

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores'; //Eloquente Não consegue achar nome da tabela por padrão. Ele tenta achar simplesmente adicionando um "s" no final. Para fazer corretamente com o Eloquente, nomeamos a tabela dessa forma.
 }
