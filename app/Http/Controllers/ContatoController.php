54c87f8fffb91cf3854500b1e02c1c78932208bb
Natzujj <gabrielnatsujake@gmail.com>
2023-06-24 09:18:35 -0300

    Entendendo o Request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,8 +6,16 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request) //Receber na action o request
+    {
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('nome');
+        echo '<br>';
+        echo $request->input('email');
         // var_dump($_POST);
+
         return view('site.contato', ['titulo' => 'Contato']);
     }
 }
