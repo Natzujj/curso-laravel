c0f34246a934547fffd0fa343fb2b44a9fdeb055
Natzujj <gabrielnatsujake@gmail.com>
2023-06-27 10:44:10 -0300

    Customizando mensagens de feedback dos erros

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,13 +17,24 @@ class ContatoController extends Controller
 
     public function salvar(Request $request){
 
-        $request->validate([
+        $regras = [
             'nome' => 'required|min:3|max:50|unique:site_contato_models',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contato_id' => 'required',
-            'mensagem' => 'required',
-        ]);
+            'mensagem' => 'required',                                   
+        ];
+
+        $feedback = [
+            'nome.min' => 'O nome deve conter pelo menos 3 caracteres',
+            'nome.max' => 'O nome deve conter no máximo 50 caracteres',
+            'nome.unique' => 'O nome já existe em nossa base de dados',
+            'motivo_contato_id.required' => 'O motivo de contato é obrigatório',
+            'required' => 'O campo :attribute é obrigatório',
+            'email' => 'O campo :attribute deve ser um email válido'
+        ];
+
+        $request->validate($regras, $feedback);
 
         SiteContatoModel::create($request->all()); 
         return redirect()->route('site.index')->with('sucesso', 'Mensagem enviada com sucesso!');
